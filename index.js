33ca1e83ccded2e4452c73cf4cc6170d2eb75e50
Aryanraj25 <119296504+Aryanraj25@users.noreply.github.com>
2026-02-03 13:57:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require ('express');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/',function(req, res){
+    fs.readdir('./files', function(err,files){
+       res.render("index", {files: files});
+    })
+    
+})
+
+app.get('/file/:filename',function(req, res){
+   fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err,filedata){
+      res.render('show', {fileName: req.params.filename, fileData: filedata});
+   })
+    
+})
+
+app.post('/create',function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('_')}.txt`, req.body.details, function(err){
+       res.redirect('/') 
+    });
+})
+
+app.get('/delete/:filename',function(req, res){
+    fs.unlink(`./files/${req.params.filename}`, function(err){
+        if(err) console.log(err);
+        res.redirect('/');
+    });
+})
+
+
+app.listen(3000);
